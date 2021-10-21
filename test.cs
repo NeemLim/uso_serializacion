5242fc4fd01bd66330923dc76b96b2a5352b4e4c
NeemLim <luismlimp@outlook.com>
2021-10-20 20:49:16 -0500

    serializacion básica

--- test.cs
+++ test.cs
@@ -11,29 +11,44 @@ namespace uso_serializacion
     class Program
     {
         static void Main(string[] args)
-        {
+        {      
+            string path = Combine(CurrentDirectory, "shapes.xml");
+            var xs = new XmlSerializer(typeof(List<circle>));
+            List<circle> circulos = new List<circle>();
             WriteLine("Hello World!");
             var circleHolder = new circle();
-            circleHolder.radium = 12;
+            circleHolder.radium = 13;
             WriteLine($"{circleHolder.radium}");
+            
+          
+            using (FileStream xmlLoad = File.Open(path, FileMode.Open))
+            {
+                // deserialize and cast the object graph into a List of Person 
+                circulos = (List<circle>)xs.Deserialize(xmlLoad);
+            }
+
+            circulos.Add(circleHolder); 
+
 
             #region serialize
-            var xs = new XmlSerializer(typeof(circle));
-            string path = Combine(CurrentDirectory, "shapes.xml");
-            using (FileStream stream = File.Create(path))
+            //using (FileStream stream = File.Create(path)) //crear archivo
+            using (FileStream stream = new FileStream(path, FileMode.Create)) //añadir al archivo
             {
-                xs.Serialize(stream, circleHolder);
+                xs.Serialize(stream, circulos);
             }
             #endregion
-
-            List<circle> circulos = new List<circle>();
+          
 
             #region deserialize
             using (FileStream xmlLoad = File.Open(path, FileMode.Open))
             {
                 // deserialize and cast the object graph into a List of Person 
-                var loadedPeople = (circle)xs.Deserialize(xmlLoad);
-                WriteLine($"{loadedPeople.radium}");
+                var loadedPeople = (List<circle>)xs.Deserialize(xmlLoad);
+                foreach (var item in loadedPeople)
+                {
+                    WriteLine("Radio es {0}",
+                      item.radium);
+                }
             }
             #endregion
 
